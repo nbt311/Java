e266d404de30b1bcaa4cc17b7f3f1142d4e0ed32
nbt311 <nbtrung0311@gmail.com>
2023-10-09 15:47:23 +0700

    Sua noi dung

--- triangle/triangle.java
+++ triangle/triangle.java
@@ -14,24 +14,30 @@ public class triangle {
         float perimeter = a + b + c;
         float p = perimeter/2;
         double acreage = Math.sqrt(p*(p-a)*(p-b)*(p-c));
-        if ( a == b && b == c){
-            System.out.println("Day la tam giac deu");
-            System.out.println("Chu vi = " + perimeter);
-            System.out.println("Dien tich = " + acreage);
-        } else if (a == b || b == c || c == a )  {
-            System.out.println("Day la tam giac can");
-            System.out.println("Chu vi = " + perimeter);
-            System.out.println("Dien tich = " + acreage);
-        } else if (a*a == b*b + c*c || b*b == a*a + c*c || c*c == a*a + b*b) {
-            System.out.println("Day la tam giac vuong");
-            System.out.println("Chu vi = " + perimeter);
-            System.out.println("Dien tich = " + acreage);
-        } else if ( (a + b > c) || (a + c > b) || ( b + c > a )) {
-            System.out.println("Day la tam giac");
-            System.out.println("Chu vi = " + perimeter);
-            System.out.println("Dien tich = " + acreage);
-        }else {
-            System.out.println("Day khong phai tam giac");
+        if ((a + b > c) && (a + c > b) && (b + c > a) &&
+                (a > 0) && (b > 0) && (c > 0)) {
+            // Kiểm tra điều các tam giác đặc biệt
+            if ((a == b) && (b == c)) {
+                System.out.println("Tam giác đều!");
+                System.out.println("Chu vi = " + perimeter);
+                System.out.println("Dien tich = " + acreage);
+            } else if ((a == b) || (a == c) || (b == c)) {
+                System.out.println("Tam giác cân!");
+                System.out.println("Chu vi = " + perimeter);
+                System.out.println("Dien tich = " + acreage);
+            } else if ((a * a == b * b + c * c) ||
+                    (b * b == a * a + c * c) ||
+                    (c * c == a * a + b * b)) {
+                System.out.println("Tam giác vuông!");
+                System.out.println("Chu vi = " + perimeter);
+                System.out.println("Dien tich = " + acreage);
+            } else {
+                System.out.println("Tam giác thường!");
+                System.out.println("Chu vi = " + perimeter);
+                System.out.println("Dien tich = " + acreage);
+            }
+        } else {
+            System.out.println("Đây không phải tam giác");
         }
     }
     }

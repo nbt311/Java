5232e9dac69610b6f532aee3edeb34d8b7761b9c
nbt311 <nbtrung0311@gmail.com>
2023-10-13 15:06:05 +0700

    them bai thuc hanh static

---
+++ Day5/Practice/StaticProperty/src/Car.java
@@ -0,0 +1,13 @@
+package src;
+
+public class Car {
+        private String name;
+        private String engine;
+        public static int numberOfCars;
+        public Car(String name, String engine) {
+            this.name = name;
+            this.engine = engine;
+            numberOfCars++;
+        }
+        // getters and setters
+}

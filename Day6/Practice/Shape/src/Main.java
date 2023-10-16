0d50fc44a73b77887488461762e272ab7d46c433
nbt311 <nbtrung0311@gmail.com>
2023-10-16 14:41:43 +0700

    them noi dung

--- Day6/Practice/Shape/src/Main.java
+++ Day6/Practice/Shape/src/Main.java
@@ -3,10 +3,13 @@ package src;
 public class Main {
     public static void main(String[] args) {
         Circle circle = new Circle();
+        System.out.println(circle);
         Rectangle rectangle = new Rectangle();
+        System.out.println(rectangle);
         Square square = new Square();
+        System.out.println(square);
         circle = new Circle(3.5, "indigo", false);
-        System.out.println(circle);;
+        System.out.println(circle);
 
         rectangle = new Rectangle(2.3, 5.8,"orange", true);
         System.out.println(rectangle);

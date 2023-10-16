14fe4d81206edcd2114912326439042cfdca59b9
nbt311 <nbtrung0311@gmail.com>
2023-10-16 17:02:53 +0700

    them bai tap

---
+++ Day5/Exercise/Studentday5/src/Student.java
@@ -0,0 +1,16 @@
+package src;
+
+public class Student {
+    private String name = "John";
+    private String classes = "C02";
+
+    public  Student(){}
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String class1) {
+        this.classes = class1;
+    }
+}

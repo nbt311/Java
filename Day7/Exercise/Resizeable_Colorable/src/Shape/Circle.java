88a4088bbf90f3acec633df8a58d48916cd62aac
nbt311 <nbtrung0311@gmail.com>
2023-10-17 16:21:08 +0700

    doi ten thu muc

---
+++ Day7/Exercise/Resizeable_Colorable/src/Shape/Circle.java
@@ -0,0 +1,54 @@
+package src.Shape;
+
+import src.IColor.Colorable;
+import src.IResize.Resizeable;
+
+public class Circle extends Shape implements Resizeable, Colorable {
+    private double radius = 1.0;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    public double getPerimeter() {
+        return 2 * radius * Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "A Circle with radius="
+                + getRadius()
+                + ", which is a subclass of "
+                + super.toString();
+    }
+
+    @Override
+    public void resize(double percent) {
+        this.radius *= (percent/100.0);
+    }
+
+    @Override
+    public void howToColor() {
+        System.out.println("Circle: Color all sides ");
+    }
+}

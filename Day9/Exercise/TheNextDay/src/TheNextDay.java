7a0c2b6e7683f3b84808a3654be5f3457ffb9547
nbt311 <nbtrung0311@gmail.com>
2023-10-19 11:56:06 +0700

    them bai tap

---
+++ Day9/Exercise/TheNextDay/src/TheNextDay.java
@@ -0,0 +1,77 @@
+package src;
+
+public class TheNextDay {
+    public static String getNextTime(int day, int month, int year){
+        if (day < 1 || day > 31){
+            return "Data Day not exactly";
+        }
+        int nextDay = day;
+        int nextMonth = month;
+        if (isaMothHave30Day(month)){
+            if (day == 30){
+                nextDay = 1;
+                nextMonth += 1;
+            }else {
+                nextDay +=1;
+            }
+        } else if (isaMothHave31Day(month)) {
+
+            if ( day == 31){
+                nextDay = 1;
+                nextMonth += 1;
+            }else {
+                nextDay +=1;
+            }
+        }else {
+            boolean isLeapYear = isLeapYear(year);
+            if (isLeapYear){
+                if (day < 29){
+                    nextDay += 1;
+                }
+                else if (day == 29){
+                    nextDay = 1;
+                    nextMonth += 1;
+                }else {
+                    return "Data Day not exactly";
+                }
+            }else {
+                if (day < 28){
+                    nextDay += 1;
+                }
+                else if (day == 28){
+                    nextDay = 1;
+                    nextMonth += 1;
+                }else {
+                    return "Data Day not exactly";
+                }
+            }
+
+        }
+
+        return year + "-" + nextMonth + "-" + nextDay;
+    }
+
+    private static boolean isLeapYear(int year) {
+        boolean isLeapYear = false;
+        boolean division4 = year % 4 == 0;
+        boolean divison100 = year % 100 == 0;
+        boolean division400 = year % 400 == 0;
+        if (division4){
+            if (divison100){
+                if(division400){
+                   isLeapYear = true;
+                }
+            }else {
+                isLeapYear = true;
+            }
+        }
+        return isLeapYear;
+    }
+
+    private static boolean isaMothHave30Day(int month) {
+        return month == 4 || month == 6 || month == 9 || month == 11;
+    }
+    private static boolean isaMothHave31Day(int month) {
+        return month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12;
+    }
+}
---
+++ Day9/Exercise/TheNextDay/test/TheNextDayTest.java
@@ -0,0 +1,92 @@
+package test;
+import org.junit.jupiter.api.Test;
+import src.TheNextDay;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TheNextDayTest {
+
+    @Test
+    public void testInputDayNotExactlyWithLessThanMin() {
+        int day = 0;
+        int month = 3;
+        int year = 2023;
+        String expected = "Data Day not exactly";
+        String actual = TheNextDay.getNextTime(day, month, year);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testInputDayNotExactlyWithMoreThanMax() {
+        int day = 32;
+        int month = 3;
+        int year = 2023;
+        String expected = "Data Day not exactly";
+        String actual = TheNextDay.getNextTime(day, month, year);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testInputDayExactlyMinWithCommonYear() {
+        int day = 1;
+        int month = 1;
+        int year = 2023;
+        String expected = "2023-1-2";
+        String actual = TheNextDay.getNextTime(day, month, year);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test2InputDayExactlyMax30DayWithCommonYear() {
+        int day = 30;
+        int month = 4;
+        int year = 2023;
+        String expected = "2023-5-1";
+        String actual = TheNextDay.getNextTime(day, month, year);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void test3InputDayExactlyMax30DayWithCommonYear() {
+        int day = 25;
+        int month = 9;
+        int year = 2023;
+        String expected = "2023-9-26";
+        String actual = TheNextDay.getNextTime(day, month, year);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testInputDayExactlyMax31DayWithCommonYear() {
+        int day = 31;
+        int month = 5;
+        int year = 2023;
+        String expected = "2023-6-1";
+        String actual = TheNextDay.getNextTime(day, month, year);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testInputDayMaxOfMothFebruaryWithCommonYear() {
+        int day = 28;
+        int month = 2;
+        int year = 2023;
+        String expected = "2023-3-1";
+        String actual = TheNextDay.getNextTime(day, month, year);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testInputDayMaxOfMothFebruaryWithLeapYear() {
+        int day = 29;
+        int month = 2;
+        int year = 2024;
+        String expected = "2024-3-1";
+        String actual = TheNextDay.getNextTime(day, month, year);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void test2InputDayMaxOfMothFebruaryWithCommon() {
+        int day = 29;
+        int month = 2;
+        int year = 2023;
+        String expected = "Data Day not exactly";
+        String actual = TheNextDay.getNextTime(day, month, year);
+        assertEquals(expected, actual);
+    }
+}

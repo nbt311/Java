0eb4c8d6fa017e596f4327c1a0029046c1d9f104
nbt311 <nbtrung0311@gmail.com>
2023-10-19 16:11:27 +0700

    them bai tap phan loai tam giac

--- Day9/Exercise/class_triangle/src/TriangleClassify.java
+++ Day9/Exercise/class_triangle/src/TriangleClassify.java
@@ -1,2 +1,19 @@
-package src;public class TriangleClassify {
+package src;
+
+public class TriangleClassify {
+
+    public static String getTriangle(int side1,int side2,int side3){
+        String message = "";
+        if ( side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2){
+                message = "Is a Triangle";
+                if ((side1 == side2 && side3 == side1)){
+                    message = "Is a Equilateral Triangle";
+               } else if (side1 == side2 || side1 == side3 || side2 == side3) {
+                    message = "Is a Iosceles Triangle";
+               }
+        }else {
+            message = "Is not Triangle";
+        }
+        return message;
+    }
 }
--- Day9/Exercise/class_triangle/test/TringaleTest.java
+++ Day9/Exercise/class_triangle/test/TringaleTest.java
@@ -1,2 +1,46 @@
-package test;public class TringaleTest {
+package test;
+import org.junit.Test;
+import src.TriangleClassify;
+
+import static org.junit.Assert.assertEquals;
+
+public class TringaleTest {
+    @Test
+    public void testIsNotTringale()  {
+        int side1 = 1;
+        int side2 = 2;
+        int side3 = 3;
+        String expected = "Is not Triangle";
+        String actual = TriangleClassify.getTriangle(side1,side2,side3);
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void testIsaTringale() {
+        int side1 = 3;
+        int side2 = 2;
+        int side3 = 4;
+        String expected = "Is a Triangle";
+        String actual = TriangleClassify.getTriangle(side1,side2,side3);
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void testIsIsoscelesTriangle(){
+        int side1 = 3;
+        int side2 = 3;
+        int side3 = 2;
+        String expected = "Is a Iosceles Triangle";
+        String actual = TriangleClassify.getTriangle(side1,side2,side3);
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void testIsEquilateralTriangle(){
+        int side1 = 3;
+        int side2 = 3;
+        int side3 = 3;
+        String expected = "Is a Equilateral Triangle";
+        String actual = TriangleClassify.getTriangle(side1,side2,side3);
+        assertEquals(expected,actual);
+    }
+
+
 }

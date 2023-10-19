66ab1fe26b4d61750e83ce978ba1fbb5722fcff5
nbt311 <nbtrung0311@gmail.com>
2023-10-19 08:06:30 +0700

    them bai tap

---
+++ Day8/Exercise/DayMonthYear/src/Test.java
@@ -0,0 +1,21 @@
+package src;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("----Hien thi ngay tiep theo----");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap ngay");
+        int day = scanner.nextInt();
+        System.out.println("Nhap thang");
+        int month = scanner.nextInt();
+        System.out.println("Nhap nam");
+        int year = scanner.nextInt();
+        Date date = new Date(day, month, year);
+        System.out.println(date.getDayMonthYear());
+
+    }
+
+
+}
